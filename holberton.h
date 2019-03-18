0775417a93e5a883be5cb8f926eaf16a389ddc6a
Yesid.dev <yesid.yag@gmail.com>
2019-03-18 15:53:53 +0000

    [Feature] adding the awesome code for the holberton.h file, and change the name to holberton.h

---
+++ holberton.h
@@ -0,0 +1,21 @@
+/* this file while content the protype functions*/
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct operations - operations for printf
+ * @operation: operation name
+ * @f: function to call
+ *
+ * Description: Longer description
+ */
+typedef struct operations
+{
+	char *operation;
+	int (*f)(va_list args);
+} operations_t;
+/* this prototype has to be declared in a header file */
+#endif/* HOLBERTON_H */
