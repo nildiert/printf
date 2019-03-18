e47cc5ebaffaf084ec728417fe95bb311c0c3bc1
nildiert <niljordan23@gmail.com>
2019-03-18 16:44:27 +0000

    [Feature] Added the function in the file printf.c

--- printf.c
+++ printf.c
@@ -12,5 +12,36 @@ int _printf(const char *format, ...)
 	operations_t types[] = {
 		{"c", printf_char},
 		{NULL, NULL}};
+
+	int j, i = 0, counter = 0;
+	va_list args;
+
+	va_start(args, format);
+	if(format == NULL)
+		return (-1);
+	while (format && format[i] != '\0')
+	{
+		while (format[i] != '%' && format[i] != '\0')
+		{
+			counter += _putchar(format[i]);
+			i++;
+		}
+		if (format[i] != '\0')
+			i++;
+		else
+			break;
+		if (format[i] == '%')
+			counter += _putchar('%');
+		else
+		{
+			for (j = 0; types[j].operation; j++)
+			{
+				if (*(types[j].operation) == format[i])
+					counter += types[j].f(args);
+			}
+		}
+		i++;
+	}
+	va_end(args);
 	return (0);
 }
