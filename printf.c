804019ecbfe4da68a4fd0dbbf71c861f277e831c
nildiert <niljordan23@gmail.com>
2019-03-18 16:08:40 +0000

    [Feature] Adding the array of instructions for print the characters

--- printf.c
+++ printf.c
@@ -1,1 +1,16 @@
 /*this file while content the printf project*/
+#include "holberton.h"
+
+/**
+ * _printf - printf function
+ * @format: format arguments to print
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	operations_t types[] = {
+		{"c", printf_char},
+		{NULL, NULL}};
+	return (0);
+}
