0e5b6ccb41cdb7b5d02129bb405aaaae6a4fa2e6
nildiert <niljordan23@gmail.com>
2019-03-19 22:51:28 +0000

    [FIX] Fixed betty style

--- printf.c
+++ printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 		{"c", printf_char}, {"i", printf_int}, {"s", printf_string},
 		{"b", printf_bin}, {"%", printf_percent}, {"d", printf_dec},
 		{"o", printf_oct}, {"x", printf_hexa}, {"X", printf_hexa_mayus},
-    {"u", printf_unsi}, {NULL, NULL}};
+		{"u", printf_unsi}, {NULL, NULL}};
 
 	int j, i = 0, counter = 0, diff = 0;
 	va_list args;
