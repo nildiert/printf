93c62a7205c32e8bd0f9b3cff483e7a5d743720b
nildiert <niljordan23@gmail.com>
2019-03-19 02:44:19 +0000

    [Fix] Fixed the file print_dec

--- print_dec.c
+++ print_dec.c
@@ -10,17 +10,5 @@ int printf_dec(va_list args)
 {
 	int n = va_arg(args, int);
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	if (n == 0)
-		_putchar('0');
-
-	if (n / 10)
-		printnumber(n / 10);
-
 	return (_putchar(n % 10 + '0'));
 }
